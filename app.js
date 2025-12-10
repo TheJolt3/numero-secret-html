b8ab3312606cf909916366cb78cdf24308314fac
TheJolt3 <brunoax05@gmail.com>
2025-12-10 07:34:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let titulo = document.querySelector('h1');
-titulo.inerHTML = 'Jogo do número secreto';
+titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';;
